962aa2cfe3887272f2627ce1e7c484a232d3748e
Edoardo Miglioli <edoardomiglioli@MacBook-Air-di-Edoardo.local>
2024-02-21 16:38:08 +0100

    adapt products page to mobile

--- client/src/components/products/ProductsHero.jsx
+++ client/src/components/products/ProductsHero.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 function ProductsHero() {
     return (
-        <section className="products-hero-container" />
+        <section className="products-hero-container">
+            <h1 className="products-hero-title"> eBikes</h1>
+        </section>
     );
 }
 
