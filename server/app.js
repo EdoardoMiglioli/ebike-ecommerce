0c261b27ec332934b33b3e308b3c96e49724fd4e
Edoardo Miglioli <edoardomiglioli@MacBook-Air-di-Edoardo.local>
2024-02-26 13:34:34 +0100

    create-register-page

--- server/app.js
+++ server/app.js
@@ -1,9 +1,22 @@
-import express from "express";
+import bcrypt from "bcrypt"
 import bodyParser from "body-parser";
+import dotenv from 'dotenv';
+import express from "express";
+import passport from "passport";
+import pg from "pg";
 
+dotenv.config();
 
 const app = express();
 const port = 5001;
+
+const db = new pg.Client({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -11,7 +24,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.json({"userId": 4});
-})
+});
+
+app.post("/register");
 
 
 app.listen(port, () => {
