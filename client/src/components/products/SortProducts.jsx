7936d901a82392f6456d9f920d5d30fef1647044
Edoardo Miglioli <edoardomiglioli@MacBook-Air-di-Edoardo.local>
2024-03-06 10:06:38 +0100

    style-sorting-products

--- client/src/components/products/SortProducts.jsx
+++ client/src/components/products/SortProducts.jsx
@@ -7,15 +7,15 @@ function SortProducts({ sortBy, onSortChange }) {
     };
     
     return (
-        <div>
-        <label htmlFor="sort-select">Sort by:</label>
-        <select id="sort-select" value={sortBy} onChange={handleSortChange}>
-          <option value="id">Featured</option>
-          <option value="name ASC">A to Z</option>
-          <option value="name DESC">Z to A</option>
-          <option value="price DESC">Price: higher to lower</option>
-          <option value="price ASC">Price: lower to higher</option>
-        </select>
+        <div className="products-sorting-container">
+            <label className="products-sorting-label" htmlFor="sort-select">Sort by</label>
+            <select className="products-sorting" id="sort-select" value={sortBy} onChange={handleSortChange}>
+              <option value="id">Featured</option>
+              <option value="name ASC">A to Z</option>
+              <option value="name DESC">Z to A</option>
+              <option value="price DESC">Price: higher to lower</option>
+              <option value="price ASC">Price: lower to higher</option>
+            </select>
         </div>
     );
 }
