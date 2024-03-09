42843d2633e5c205bc8a6ce217d04380d79bf89d
Edoardo Miglioli <edoardomiglioli@MacBook-Air-di-Edoardo.local>
2024-03-09 07:33:51 +0100

    fix-google-0auth

--- client/src/components/external-login-buttons/GitHubButton.jsx
+++ client/src/components/external-login-buttons/GitHubButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function GitHubButton(props) {
     return (
-        <div className="external-login-button github" >
+        <div className="external-login-button github" onClick={props.onClick} >
             <GitHubIcon alt="GitHub logo" className="login-icon" />
             GitHub
         </div>
