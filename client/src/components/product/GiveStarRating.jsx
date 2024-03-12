d2e4393b4579fc2258a41d6762e79c627ddfb6c0
Edoardo Miglioli <edoardomiglioli2008@gmail.com>
2024-03-12 10:01:19 +0100

    stars-rating-ui

---
+++ client/src/components/product/GiveStarRating.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+function GiveStarRating({ initialRating, onRatingChange }) {
+  const [rating, setRating] = useState(initialRating);
+
+  const handleStarClick = (value) => {
+    setRating(value);
+    if (onRatingChange) {
+      onRatingChange(value);
+    }
+  };
+
+  return (
+    <div className="give-rating-stars">
+      {[1, 2, 3, 4, 5].map((value) => (
+        <Star key={value} filled={value <= rating} onClick={() => handleStarClick(value)} />
+      ))}
+    </div>
+  );
+}
+
+function Star({ filled, onClick }) {
+  return (
+    <span className="give-rating-star" style={{ color: filled ? '#204969' : 'lightgray', cursor: 'pointer' }} onClick={onClick}>
+      &#9733;
+    </span>
+  );
+}
+
+export default GiveStarRating;
